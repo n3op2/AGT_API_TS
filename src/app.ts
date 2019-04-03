12ae95162c499960f97f98fc0b449717eda8e72b
n3op2 <paulius.michelevicius@gmail.com>
2019-04-03 22:30:48 +0100

    Playing around...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Request, Response } from 'express';
 import * as http from 'http';
 import config from 'config';
 import bodyParser from 'body-parser';
@@ -8,25 +7,43 @@ import { Auth } from './lib/auth';
 const PORT = config.get('server.port');
 const auth = new Auth();
 const app = express();
+
 app.set('Access-Control-Allow-Origin', '*');
 app.use(bodyParser.json());
 
-auth.generateToken({}, (err, token) => {
-  console.log(token)
-});
-
-app.get('/', (req: Request, res: Response) => {
-
-});
-/*
-const generate = async () => {
+const testObject: object = {
+  name: 'paul',
+  email: 'email@email.com'
 };
-*/
+
+// Middleware
+app.use('/api', (req, res, next) => {
+  // TODO: Rename header to something that makes more sense
+  if (req.headers.token && typeof req.headers.token === 'string') {
+    const token: string = req.headers.token
+    auth.verifyToken(token, (err, user) => {
+      if (err) res.send(err);
+      req.user = user;
+      return next();
+    });
+  }
+  res.send('Token was not provided...');
+});
+
+app.get('/login', (req, res) => {
+  // We assume that we have successfully
+  // authenticated...
+  auth.generateToken(testObject, (err, token) => {
+    // TODO: Logger!
+    if (err) return res.send(err);
+    res.send({ Token: token });
+  });
+});
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log('Server has starter...');
+  console.log('Server has started...');
 });
 
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken';
 import conifg from 'config';
 import uuidv4 from 'uuid';
+import { Request, Response } from 'express';
 
 const options: Object = {
   expiresIn: '1h',
 };
 
 type tokenCb = (err: object | null, token: string | null) => void;
+type verifyCb = (err: object | null, user: string | object | null) => void;
 
 export class Auth {
   private pass: string;
@@ -17,6 +19,12 @@ export class Auth {
     await jwt.sign(user, this.pass, {}, (err, token) => {
       if (err) return cb(err, null);
       cb(null, token); 
+    });
+  };
+  public verifyToken = async (token: string, cb: verifyCb) => {
+    await jwt.verify(token, this.pass, (err, user) => {
+      if (err) return cb(err, null);
+      cb(null, user);
     });
   };
 }
